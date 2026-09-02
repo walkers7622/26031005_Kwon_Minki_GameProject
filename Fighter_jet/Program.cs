749e6d0fb5d3bdcda9788996ef6bce75bfefa365
walkers7622 <kwon070726@gmail.com>
2026-09-02 14:34:01 +0900

    게임 프로젝트 생성

---
+++ Fighter_jet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fighter_jet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
