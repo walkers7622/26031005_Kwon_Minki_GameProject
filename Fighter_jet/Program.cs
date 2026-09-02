7f5dde743a7b3a7d971fea9e7f7fe842b3487bce
walkers7622 <kwon070726@gmail.com>
2026-09-02 14:54:42 +0900

    최종

--- Fighter_jet/Program.cs
+++ Fighter_jet/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Fighter_jet
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
 }
