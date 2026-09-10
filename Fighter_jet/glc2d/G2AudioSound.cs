61cad52463c4296c077c53e9ec104f38bb9d6c61
kwon_walker <kwon070726@gmail.com>
2026-09-10 18:23:06 +0900

    오디오 사운드 코드 수정

--- Fighter_jet/glc2d/G2AudioSound.cs
+++ Fighter_jet/glc2d/G2AudioSound.cs
@@ -2,93 +2,68 @@
 // Author: 3dapi (https://github.com/3dapi)
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-using Vortice.Multimedia;
 using Vortice.XAudio2;
 
-class G2AudioSound : IDisposable
+class G2AudioContext : IDisposable
 {
-	// WAV 파일 데이터를 저장하는 내부 클래스.
-	private class WavData
-	{
-		public WaveFormat Format = null!;
-		public AudioBuffer Buffer = null!;
-		public int Count;
-	}
-	private static readonly Dictionary<string, WavData> SoundList = new();
+    public static G2AudioContext? Instance { get; private set; }
 
-	public string FilePath { get; }
+    // XAudio2 인스턴스.
+    public IXAudio2? Audio { get; }
+    // XAudio2 마스터링 보이스 공유.
+    public IXAudio2MasteringVoice? MasteringVoice { get; }
 
-	// WAV 파일 데이터와 관련된 필드.
-	private WavData? _wavData;
-	private readonly AudioBuffer _playBuffer;
-	private readonly IXAudio2SourceVoice _sourceVoice;
+    public G2AudioContext()
+    {
+        if (Instance != null)
+        {
+            throw new InvalidOperationException("G2AudioContext instance already exists.");
+        }
+        if (IsAudioOutputAvailable())
+        {
+            IXAudio2? audio = null;
+            IXAudio2MasteringVoice? masteringVoice = null;
+            try
+            {
+                audio = XAudio2.XAudio2Create();
+                masteringVoice = audio.CreateMasteringVoice();
+                Audio = audio;
+                MasteringVoice = masteringVoice;
+            }
+            catch
+            {
+                masteringVoice?.Dispose();
+                audio?.Dispose();
+                throw;
+            }
+        }
+        Instance = this;
+    }
 
-	public G2AudioSound(string filePath)
-	{
-		if (string.IsNullOrWhiteSpace(filePath))
-		{
-			throw new ArgumentException("G2AudioSound::파일 경로가 비어 있습니다.", nameof(filePath));
-		}
-		var audio = G2AudioContext.Instance?.Audio ?? throw new InvalidOperationException("G2AudioSound::G2AudioContext instance is not initialized.");
-		FilePath = G2Util.FindFilePath(filePath);
-		if (SoundList.TryGetValue(FilePath, out WavData? wavData))
-		{
-			wavData.Count++;
-			_wavData = wavData;
-		}
-		else
-		{
-			if (!File.Exists(FilePath))
-			{
-				throw new FileNotFoundException($"G2AudioSound::{FilePath} 파일을 찾을 수 없습니다.");
-			}
-			using SoundStream stream = new SoundStream(File.OpenRead(FilePath));
-			if (stream.Format == null)
-			{
-				throw new InvalidDataException($"G2AudioSound::{FilePath} 파일의 포맷 정보를 읽을 수 없습니다.");
-			}
-			using var dataStream = stream.ToDataStream();
+    public void Dispose()
+    {
+        MasteringVoice?.Dispose();
+        Audio?.Dispose();
+        Instance = null;
+    }
 
-			_wavData = new WavData
-			{
-				Format = stream.Format,
-				Buffer = new AudioBuffer(dataStream),
-				Count = 1
-			};
-			SoundList.Add(FilePath, _wavData);
-		}
-		_playBuffer = new AudioBuffer(_wavData.Buffer.AudioDataPointer, _wavData.Buffer.AudioBytes, _wavData.Buffer.Flags);
-		_sourceVoice = audio.CreateSourceVoice(_wavData.Format);
-	}
-
-	public void Play(bool isLooping = false)
-	{
-		Stop();
-		_playBuffer.LoopCount = isLooping ? (uint)XAudio2.LoopInfinite : 0;
-		_sourceVoice.SubmitSourceBuffer(_playBuffer);
-		_sourceVoice.Start();
-	}
-
-	public bool IsPlaying()
-	{
-		return _sourceVoice.State.BuffersQueued > 0;
-	}
-
-	public void Stop()
-	{
-		_sourceVoice.Stop();
-		_sourceVoice.FlushSourceBuffers();
-	}
-
-	public void Dispose()
-	{
-		_sourceVoice.Dispose();
-		_wavData!.Count--;
-		if (_wavData.Count == 0)
-		{
-			_wavData.Buffer.Dispose();
-			SoundList.Remove(FilePath);
-		}
-		_wavData = null;
-	}
+    // 활성화된 오디오 출력 장치 확인
+    protected bool IsAudioOutputAvailable()
+    {
+        try
+        {
+            // Core Audio API 장치 열거자 생성.
+            var enumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            // 출력(Render) 장치 중 활성화(Active) 상태 장치 리스트.
+            var devices = enumerator.EnumerateAudioEndPoints(
+                NAudio.CoreAudioApi.DataFlow.Render,
+                NAudio.CoreAudioApi.DeviceState.Active);
+            // 활성화된 스피커가 1개 이상이면 true
+            return devices.Count > 0;
+        }
+        catch
+        {
+        }
+        return false;
+    }
 }
